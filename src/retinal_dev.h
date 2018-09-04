9840f738940a6fbe7af71db5cad55cf49b53e2d3
JeandeMontigny <J.De-Montigny2@newcastle.ac.uk>
2018-09-04 16:43:05 +0100

    WIP implement swc export

--- src/retinal_dev.h
+++ src/retinal_dev.h
@@ -97,7 +97,7 @@ struct RGC_dendrite_growth_test : public BaseBiologyModule {
       auto ne = sim_objectNe.GetSoPtr();
 
       if (ne->IsTerminal() && ne->GetDiameter() >= 0.6
-          && ne->GetNeuronSomaOfNeurite()->GetInternalClock() > 1600) {
+          && ne->GetNeuronSomaOfNeurite()->GetInternalClock() > 1601) {
         if (!init_) {
           dg_on_RGCguide_ = rm->GetDiffusionGrid("on_substance_RGC_guide");
           dg_off_RGCguide_ = rm->GetDiffusionGrid("off_substance_RGC_guide");
@@ -610,13 +610,13 @@ inline void position_exporteur(int i) {
   stringstream sstmOff;
   stringstream sstmOnOff;
   stringstream sstmAll;
-  sstmOn << Concat(Param::kOutputDir, "/cellPosition/on_t") << i
+  sstmOn << Concat(Param::kOutputDir, "/cells_position/on_t") << i
          << "_seed" << seed << ".txt";
-  sstmOff << Concat(Param::kOutputDir, "/cellPosition/off_t") << i
+  sstmOff << Concat(Param::kOutputDir, "/cells_position/off_t") << i
           << "_seed" << seed << ".txt";
-  sstmOnOff << Concat(Param::kOutputDir, "/cellPosition/onOff_t") << i
+  sstmOnOff << Concat(Param::kOutputDir, "/cells_position/onOff_t") << i
             << "_seed" << seed << ".txt";
-  sstmAll << Concat(Param::kOutputDir, "/cellPosition/all_t") << i
+  sstmAll << Concat(Param::kOutputDir, "/cells_position/all_t") << i
             << "_seed" << seed << ".txt";
 
   string fileNameOn = sstmOn.str();
@@ -664,6 +664,76 @@ inline void position_exporteur(int i) {
   positionFileOnOff.close();
   positionFileAll.close();
 }  // end position_exporteur
+
+int labelSWC_;
+template <typename TSimulation = Simulation<>>
+inline void morpho_exporteur() {
+  auto* sim = TSimulation::GetActive();
+  auto* rm = sim->GetResourceManager();
+  int seed = sim->GetRandom()->GetSeed();
+  auto my_cells = rm->template Get<MyCell>();
+
+  for (int cellNum = 0; cellNum < my_cells->size(); cellNum++) {
+    auto& cell = (*my_cells)[cellNum];
+    int thisCellType = cell.GetCellType();
+    auto cellPosition = cell.GetPosition();
+    ofstream swcFile;
+    string swcFileName = Concat(Param::kOutputDir, "/swc_files/cell",
+                          cellNum, "_seed", seed, ".swc").c_str();
+    swcFile.open(swcFileName);
+
+    labelSWC_=1;
+    // swcFile << labelSWC_ << " 1 " << cellPosition[0] << " "
+    //         << cellPosition[1]  << " " << cellPosition[2] << " "
+    //         << cell->GetDiameter()/2 << " -1";
+    swcFile << labelSWC_ << " 1 0 0 0 " << cell->GetDiameter()/2 << " -1";
+
+    for (auto& ne : cell->GetDaughters()) {
+      swcFile << swc_neurites(ne, 1, cellPosition);
+    } // end for neurite in cell
+    swcFile.close();
+  } // end for cell in simulation
+} // end morpho_exporteur
+
+inline string swc_neurites(const MyNeurite ne, int labelParent, array<double, 3> somaPosition) {
+  array<double, 3> nePosition = ne->GetPosition();
+  nePosition[0] = nePosition[0] - somaPosition[0];
+  nePosition[1] = nePosition[1] - somaPosition[1];
+  nePosition[2] = nePosition[2] - somaPosition[2];
+
+  string temps;
+  // if branching point
+  if (ne->GetDaughterRight() != nullptr) {
+    labelSWC_++;
+    //FIXME: segment indice should be 5, no 3. If set here,
+    // it's not the actual branching point, but the following segment
+    // need to run correction.py to correct file
+    temps = Concat(temps,"\n", labelSWC_, " 3 ",
+            nePosition[0], " ", nePosition[1], " ", nePosition[2], " ",
+            ne->GetDiameter()/2, " ", labelParent,
+            swc_neurites(ne->GetDaughterRight(),
+                         labelSWC_, somaPosition)).c_str();
+  }
+  // if is straigh dendrite
+  if (ne->GetDaughterLeft() != nullptr) {
+    labelSWC_++;
+    temps = Concat(temps,"\n", labelSWC_, " 3 ",
+            nePosition[0], " ", nePosition[1], " ", nePosition[2], " ",
+            ne->GetDiameter()/2, " ", labelParent,
+            swc_neurites(ne->GetDaughterLeft(),
+                         labelSWC_, somaPosition)).c_str();
+  }
+  // if ending point
+  if (ne->GetDaughterLeft() == nullptr && ne->GetDaughterRight() == nullptr) {
+    labelSWC_++;
+    temps = Concat(temps,"\n", labelSWC_, " 6 ",
+            nePosition[0], " ", nePosition[1], " ", nePosition[2], " ",
+            ne->GetDiameter()/2, " ", labelParent).c_str();
+  }
+
+  return temps;
+} // end swc_neurites
+
 
 // RI computation
 inline double computeRI(vector<array<double, 3>> coordList) {
@@ -737,7 +807,7 @@ inline int Simulate(int argc, const char** argv) {
   auto* param = simulation.GetParam();
 
   // number of simulation steps
-  int maxStep = 3000;
+  int maxStep = 2200;
   // Create an artificial bounds for the simulation space
   int cubeDim = 500;
   int num_cells = 4400; // 4400
@@ -803,19 +873,14 @@ inline int Simulate(int argc, const char** argv) {
   // prepare export
   ofstream outputFile;
   // delete previous simulation export
-  if (writePositionExport) {
-    if (system(Concat("rm -r ", Param::kOutputDir, "/cellPosition/*").c_str())) {
-      cout << "error in " << Param::kOutputDir
-           << "/cellPosition folder deletion" << endl;
-    }
-    if(system(Concat("mkdir -p ", Param::kOutputDir, "/cellPosition").c_str())) {
-      cout << "error in " << Param::kOutputDir
-           << "/cellPosition folder creation" << endl;
-    }
+  if (writePositionExport && system(Concat("mkdir -p ",
+      Param::kOutputDir, "/cells_position").c_str())) {
+    cout << "error in " << Param::kOutputDir
+         << "/cells_position folder creation" << endl;
   }
   // create RI export file
   if (writeRI) {
-    outputFile.open("output/cellPosition/RI_" + to_string(mySeed) + ".txt");
+    outputFile.open("output/cells_position/RI_" + to_string(mySeed) + ".txt");
   }
 
   // 4. Run simulation for maxStep timesteps
